13543e718ec6cdb0569ee0d75f5a6dcdefd14911
AntonHedlundFG <Anton.Hedlund@edu.futuregames.nu>
2023-07-07 14:23:43 +0200

    Initial commit

---
+++ Source/InvisiblePlatformer/InvisiblePlatformerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "InvisiblePlatformerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INVISIBLEPLATFORMER_API AInvisiblePlatformerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
